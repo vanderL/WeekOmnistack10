9408614a29a06bb4e06aeefc950c6187b8494da0
Vander Lima <vnd.vander@gmail.com>
2020-12-23 17:54:13 -0300

    Creating the application base

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const routes = require('./routes')
+
+const app = express()
+
+mongoose.connect('mongodb://localhost/semanaOmnistack10', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}
+).then(() => console.log('connection succesful mongodb'))
+  .catch((err) => console.error('Deu erro: '+ err));
+
+
+app.use(express.json())
+app.use(routes)
+
+
+app.listen(3333)
---
+++ backend/src/routes.js
@@ -0,0 +1,28 @@
+const { Router } = require('express')
+const axios = require('axios')
+const Dev = require('./models/Dev')
+
+const routes = Router()
+
+routes.post('/devs', async (request, response) => {
+    const { github_username, techs, latitude, longitude } = request.body
+    const ApiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
+    const {name = login, avatar_url, bio} = ApiResponse.data
+    const techsArray = techs.split(',').map(tech => tech.trim())
+
+    const location = {
+        type: 'Point'
+    }
+
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray
+    })
+
+    return response.json(dev)
+})
+
+module.exports = routes
